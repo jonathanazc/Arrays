4a6ae1d192e660eabe5d8d7874207af427a62fd8
Jonathan Keith Azcona <jonathanazc@users.noreply.github.com>
2020-02-06 16:29:25 -0500

    Initial commit

---
+++ Arrays/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Decleares and initiallizes the array
+            //if multiple things betw. brackets, put a ','
+            int[,,] theArray = new int[5, 10, 15];
+
+            //displays
+            Console.WriteLine("The array has {0} numbers in it", theArray.Rank); //the '.Rank' displays the number of dimensions of an array
+            Console.ReadLine();
+
+
+        }
+    }
+}
